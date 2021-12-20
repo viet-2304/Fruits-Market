7a0bae443209301ffa5b024ce9fe0689da92d869
viet-2304 <vietphama7@gmail.com>
2021-12-20 18:29:06 +0700

    fix coding convention for scss file

--- src/components/body/countdown/Countdown.js
+++ src/components/body/countdown/Countdown.js
@@ -2,26 +2,24 @@ import React, { useEffect, useState, useRef } from "react"
 import './Countdown.scss'
 
 const Countdown = () => {
-    const [timerDays, setTimerDays]= useState('00');
-    const [timerHours, setTimerHours]= useState('00');
-    const [timerMinutes, setTimerMinutes]= useState('00');
-    const [timerSeconds, setTimerSeconds]= useState('00');
+    const [timerDays, setTimerDays] = useState('00')
+    const [timerHours, setTimerHours] = useState('00')
+    const [timerMinutes, setTimerMinutes] = useState('00')
+    const [timerSeconds, setTimerSeconds] = useState('00')
 
-    let interval =useRef()
+    let interval = useRef()
 
     const startTimer = () => {
-        const countdownDate= new Date('May 30, 2022 00:00:00').getTime()
+        const countdownDate = new Date('May 30, 2022 00:00:00').getTime()
 
         interval = setInterval(() => { 
             const now = new Date().getTime()
             const distance = countdownDate-now
-
-            const days = Math.floor( distance / (1000 * 60 * 60 * 24) )
-            const hours = Math.floor( (distance % (1000 * 60 * 60 *24) / (1000 * 60 * 60)) )
-            const minutes = Math.floor(( distance % (1000 * 60 * 60) / (1000 * 60)) )
-            const seconds = Math.floor(( distance % (1000 * 60) / 1000 ))
-
-            if( distance < 0) {
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24) )
+            const hours = Math.floor(distance % (1000 * 60 * 60 *24) / (1000 * 60 * 60) )
+            const minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60)) 
+            const seconds = Math.floor(distance % (1000 * 60) / 1000 )
+            if (distance < 0) {
                 clearInterval(interval.current)
             } 
             else {
@@ -30,19 +28,17 @@ const Countdown = () => {
                 setTimerMinutes(minutes)
                 setTimerSeconds (seconds)
             }
-
         }, 1000);
     }
-        useEffect(() => {
 
-            startTimer()
-            return () => {
-                clearInterval(interval)
-            }
-        })
+    useEffect(() => {
+        startTimer()
+        return () => {
+            clearInterval(interval)
+        }
+    })
 
     return (
-
         <div className="timer-contains">
             <div className="timer-item">
                 <div className="timer-number">
